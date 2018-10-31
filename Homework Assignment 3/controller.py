dc3940175b6ffa16bfe380bccfbd85ef63706915
Francesco Milano <francesco.milano172@gmail.com>
2018-10-31 20:37:06 +0100

    Submission homework 3 frankbot (fixed indentation error)

--- Homework Assignment 3/controller.py
+++ Homework Assignment 3/controller.py
@@ -5,29 +5,28 @@ import rospy
 class Controller():
 
     def __init__(self):
+      # Whether to cross-validate or not
+      self.cross_validation_mode = False
 
-	# Whether to cross-validate or not
-	self.cross_validation_mode = False
-	
-        # Gains for controller
-        self.k_P = 4
-	self.k_I = 5
-	self.k_D = 0.01
+      # Gains for controller
+      self.k_P = 4
+      self.k_I = 5
+      self.k_D = 0.01
 
-	# Extremal values for cross validation
-	self.k_P_start = 3
-	self.k_P_end = 6
-	self.k_I_start = 4 
-	self.k_I_end = 8
-	
-	self.total_time = 0
-	self.time_for_k_I_step = 20
+      # Extremal values for cross validation
+      self.k_P_start = 3
+      self.k_P_end = 6
+      self.k_I_start = 4
+      self.k_I_end = 8
 
-	# For D
-	self.last_err = 0	
+      self.total_time = 0
+      self.time_for_k_I_step = 20
 
-	# For I
-	self.total_err = 0
+      # For D
+      self.last_err = 0
+
+      # For I
+      self.total_err = 0
 
     # Inputs:   d_est   Estimation of distance from lane center (positve when
     #                   offset to the left of driving direction) [m]
@@ -41,36 +40,36 @@ class Controller():
     # Output:   v_out       forward linear velocity of Duckiebot [m/s]
     #           omega_out   angular velocity of Duckiebot [rad/s]
     def getControlOutput(self, d_est, phi_est, d_ref, phi_ref, v_ref, t_delay, dt_last):
-        if self.cross_validation_mode:
-          # Perform a rough cross validation on k_P and k_I
-          self.total_time += dt_last
-	  if self.total_time % self.time_for_k_I_step < 0.1:
-	    if self.k_I == self.k_I_end:
-	      self.k_P += 1
-	      self.k_I = self.k_I_start
-	    else:
-	      self.k_I += 1
-	    rospy.loginfo('Using k_P = {0}, k_I = {1}'.format(self.k_P, self.k_I))
-	  elif 1 < (self.total_time % self.time_for_k_I_step) < 6: # Move duckie!
-	    self.total_err = 0
-	    self.last_err = 0
-	    return (0, 0)
+      if self.cross_validation_mode:
+        # Perform a rough cross validation on k_P and k_I
+        self.total_time += dt_last
+        if self.total_time % self.time_for_k_I_step < 0.1:
+          if self.k_I == self.k_I_end:
+            self.k_P += 1
+            self.k_I = self.k_I_start
+          else:
+            self.k_I += 1
+          rospy.loginfo('Using k_P = {0}, k_I = {1}'.format(self.k_P, self.k_I))
+        elif 1 < (self.total_time % self.time_for_k_I_step) < 6: # Move duckie!
+          self.total_err = 0
+          self.last_err = 0
+          return (0, 0)
 
-        # Calculate the output y
-        ref =   (6 * d_ref + 1 * phi_ref)
-        y =     (6 * d_est + 1 * phi_est)
-        err = ref - y
-	self.last_err = err
-	
-        # PID controller
-        C_P = self.k_P * err + 1/self.k_I * self.total_err + self.k_D * (err-self.last_err)/dt_last
+      # Calculate the output y
+      ref = (6 * d_ref + 1 * phi_ref)
+      y = (6 * d_est + 1 * phi_est)
+      err = ref - y
+      self.last_err = err
 
-        omega = C_P
+      # PID controller
+      C_P = self.k_P * err + 1/self.k_I * self.total_err + self.k_D * (err-self.last_err)/dt_last
 
-	self.total_err += err
+      omega = C_P
 
-        # Declaring return values
-        omega_out = omega
-        v_out = v_ref
+      self.total_err += err
 
-        return (v_out, omega_out)
+      # Declaring return values
+      omega_out = omega
+      v_out = v_ref
+
+      return (v_out, omega_out)
